e0a6cad62c29f5c2f78cef3471b3b8ff140273b1
firgia <mochamadfirgia@gmail.com>
2022-04-29 14:18:18 +0700

    seperating page

--- concentric/lib/main.dart
+++ concentric/lib/main.dart
@@ -1,10 +1,9 @@
-import 'package:concentric/widget/card_planet.dart';
-import 'package:concentric_transition/concentric_transition.dart';
+import 'package:concentric/pages/onboarding_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lottie/lottie.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(const MyApp());
@@ -19,55 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
-
-  final data = [
-    CardPlanetData(
-      title: "observe",
-      subtitle:
-          "The night sky has much to offer to those who seek its mystery.",
-      image: const AssetImage("assets/images/img-1.png"),
-      backgroundColor: const Color.fromRGBO(0, 10, 56, 1),
-      titleColor: Colors.pink,
-      subtitleColor: Colors.white,
-      background: LottieBuilder.asset("assets/animation/bg-1.json"),
-    ),
-    CardPlanetData(
-      title: "imagine",
-      subtitle: "An endless number of galaxies means endless possibilities.",
-      image: const AssetImage("assets/images/img-2.png"),
-      backgroundColor: Colors.white,
-      titleColor: Colors.purple,
-      subtitleColor: const Color.fromRGBO(0, 10, 56, 1),
-      background: LottieBuilder.asset("assets/animation/bg-2.json"),
-    ),
-    CardPlanetData(
-      title: "stargaze",
-      subtitle: "The sky dome is a beautiful graveyard of stars.",
-      image: const AssetImage("assets/images/img-3.png"),
-      backgroundColor: const Color.fromRGBO(71, 59, 117, 1),
-      titleColor: Colors.yellow,
-      subtitleColor: Colors.white,
-      background: LottieBuilder.asset("assets/animation/bg-3.json"),
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ConcentricPageView(
-        colors: data.map((e) => e.backgroundColor).toList(),
-        itemCount: data.length,
-        itemBuilder: (int index, double value) {
-          return CardPlanet(data: data[index]);
-        },
-      ),
+      home: OnboardingPage(),
     );
   }
 }
